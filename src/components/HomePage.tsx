3b0f6c6f690db02a463994c57b122a71132aad71
Brand0nLe <93brandonle@gmail.com>
2023-05-13 17:16:49 -0700

    HomePage Bootstrap

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import RestaurantCard from './RestaurantCard';
 import SearchBar from './SearchBar';
 
-const HomePage = () => {
-    const [results, setResults] = useState([]);
-  
-    const handleSearch = (newResults) => {
-      setResults(newResults);
-    };
-  
-    return (
-      <main>
-        <SearchBar onSearch={handleSearch} />
-        <div>
-          {results.map((restaurant) => (
-            <RestaurantCard
-              key={restaurant.id}
-              restaurant={restaurant}
-              onBookmark={/* Implement bookmarking logic here. */}
-            />
-          ))}
-        </div>
-      </main>
-    );
+interface Restaurant {
+  id: string;
+  // Add other properties...
+}
+
+const HomePage: React.FC = () => {
+  const [results, setResults] = useState<Restaurant[]>([]);
+
+  const handleSearch = (newResults: Restaurant[]) => {
+    setResults(newResults);
   };
-  
-  export default HomePage;
-  
+
+  const handleBookmark = (restaurant: Restaurant) => {
+    // Implement bookmarking logic here.
+  };
+
+  return (
+    <main>
+      <Container>
+        <SearchBar onSearch={handleSearch} />
+        <Row>
+          {results.map((restaurant) => (
+            <Col lg={4} md={6} sm={12} key={restaurant.id}>
+              <RestaurantCard
+                restaurant={restaurant}
+                onBookmark={handleBookmark}
+                showBookmark={true} // Optional: To show bookmark button in HomePage
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default HomePage;
