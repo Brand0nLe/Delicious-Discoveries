700fdc0bf709cc082e8ddc28b03ef6f969632511
Brand0nLe <93brandonle@gmail.com>
2023-05-13 17:39:03 -0700

    API key + add to git ignore

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,37 @@
 import React, { useState } from 'react';
 
-const SearchBar = ({ onSearch }) => {
+interface Props {
+  onSearch: (results: any[]) => void;
+}
+
+const SearchBar: React.FC<Props> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Make Yelp API request here, then pass the results to onSearch.
-    onSearch([]);
+
+    try {
+      const response = await fetch(
+        `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=YOUR_LOCATION`,
+        {
+          headers: {
+            Authorization: 'Bearer YOUR_YELP_API_KEY',
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const results = data.businesses;
+        onSearch(results);
+      } else {
+        console.error('Error occurred while fetching Yelp API:', response.statusText);
+        onSearch([]);
+      }
+    } catch (error) {
+      console.error('Error occurred while fetching Yelp API:', error);
+      onSearch([]);
+    }
   };
 
   return (
