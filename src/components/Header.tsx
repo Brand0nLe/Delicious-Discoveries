a721570206c6bcd40d1a16511ce064725646ff3a
Brand0nLe <93brandonle@gmail.com>
2023-05-13 17:14:31 -0700

    navbar layout with themeswitch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-  // Add theme switching logic here.
+const Header: React.FC = () => {
+  const handleThemeChange = (theme: string) => {
+    // Implement theme switching logic here.
+  };
 
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/bookmarks">Bookmarks</Link>
-        // Add theme selector here.
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            Culinary Compass
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/bookmarks" className="nav-link">
+                  Bookmarks
+                </Link>
+              </li>
+              <li className="nav-item">
+                <select
+                  className="form-control"
+                  onChange={(e) => handleThemeChange(e.target.value)}
+                >
+                  <option value="light">Light Theme</option>
+                  <option value="dark">Dark Theme</option>
+                  <option value="ice-cream">Ice Cream Theme</option>
+                  {/* Add other theme options */}
+                </select>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
